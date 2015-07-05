edf57f89a36d626175111dff21c309381e999d0c
Hector Zarco <hezarco@gmail.com>
2015-07-05 15:25:49 +0200

    Adding some mocks...

---
+++ app/mirage/config.js
@@ -0,0 +1,116 @@
+export default function() {
+  this.get('/api/videos', function() {
+    return {
+      videos: [
+        {
+          id: 1,
+          name: 'Jesus Fernandez Bones video part',
+          cover: 'https://igcdn-photos-b-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11240718_1442937302685265_811948162_n.jpg',
+          src: 'https://scontent-mad1-1.cdninstagram.com/hphotos-xaf1/t50.2886-16/11229028_966460406711103_1363911861_n.mp4'
+        },
+        {
+          id: 2,
+          name: 'Shaunhover flatground tricks',
+          cover: 'https://igcdn-photos-h-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11258053_1586107534976447_1284903766_n.jpg',
+          src: 'https://igcdn-videos-e-19-a.akamaihd.net/hphotos-ak-xfa1/t50.2886-16/11229035_971906422833059_1407736680_n.mp4'
+        },
+        {
+          id: 3,
+          name: 'Nice trick☛ @niilonikkanen',
+          cover: 'https://igcdn-photos-d-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11236122_949022468483003_43650478_n.jpg',
+          src: 'https://scontent-mad1-1.cdninstagram.com/hphotos-xaf1/t50.2886-16/11250376_882414788482009_1950791821_n.mp4'
+        },
+        {
+          id: 4,
+          name: 'Christian Sanchez mira mira tricks',
+          cover: 'https://igcdn-photos-e-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11246926_1468058723485996_1327045361_n.jpg',
+          src: 'https://scontent-mad1-1.cdninstagram.com/hphotos-xaf1/t50.2886-16/11214786_367446683463254_1504692181_n.mp4'
+        },
+        {
+          id: 5,
+          name: 'Some macba fun',
+          cover: 'https://igcdn-photos-c-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11232859_473250199498586_813102419_n.jpg',
+          src: 'https://igcdn-videos-g-2-a.akamaihd.net/hphotos-ak-xaf1/t50.2886-16/11214516_482754115234202_74106415_n.mp4'
+        },
+        {
+          id: 6,
+          name: 'Fran Molina has pop!',
+          cover: 'https://igcdn-photos-e-a.akamaihd.net/hphotos-ak-xap1/t51.2885-15/11201658_772833612829916_161248747_n.jpg',
+          src: 'https://scontent-mad1-1.cdninstagram.com/hphotos-xaf1/t50.2886-16/11237361_808967109193656_713752479_n.mp4'
+        },
+        {
+          id: 7,
+          name: 'Here☛ @timmyjammz 🎬From @codycepeda 👐 #TCMT #dewtourPUSH',
+          cover: 'https://igcdn-photos-a-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11236284_670295223098984_561746941_n.jpg',
+          src: 'https://igcdn-videos-a-11-a.akamaihd.net/hphotos-ak-xfa1/t50.2886-16/11214803_1389923104669291_204317956_n.mp4'
+        },
+        {
+          id: 8,
+          name: 'Xavi Benguerel #tbt',
+          cover: 'https://igcdn-photos-a-a.akamaihd.net/hphotos-ak-xpa1/t51.2885-15/11190875_357858841080368_608102022_n.jpg',
+          src: 'https://igcdn-videos-a-5-a.akamaihd.net/hphotos-ak-xaf1/t50.2886-16/11214399_427659494081125_915157407_n.mp4'
+        },
+        {
+          id: 9,
+          name: 'Some tricks at #Barceloneta',
+          cover: 'https://igcdn-photos-a-a.akamaihd.net/hphotos-ak-xpt1/t51.2885-15/11208533_756981704416072_1857171790_n.jpg',
+          src: 'https://igcdn-videos-e-4-a.akamaihd.net/hphotos-ak-xaf1/t50.2886-16/11213139_878344518874004_231906001_n.mp4'
+        },
+        {
+          id: 10,
+          name: '@maxgeronzi 😃👐 @vincentjugnet🎥 #GYPSYLIFEFLASHBACK #BarcelonaSkateboarding',
+          cover: 'https://igcdn-photos-b-a.akamaihd.net/hphotos-ak-xpa1/t51.2885-15/11184617_807866679291353_1531308800_n.jpg',
+          src: 'https://scontent-mad1-1.cdninstagram.com/hphotos-xap1/t50.2886-16/11200527_879632998756750_1140061383_n.mp4'
+        },
+        {
+          id: 11,
+          name: 'Shaunhover flatground tricks',
+          cover: 'https://igcdn-photos-h-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11258053_1586107534976447_1284903766_n.jpg',
+          src: 'https://igcdn-videos-e-19-a.akamaihd.net/hphotos-ak-xfa1/t50.2886-16/11229035_971906422833059_1407736680_n.mp4'
+        },
+        {
+          id: 12,
+          name: 'Shaunhover flatground tricks',
+          cover: 'https://igcdn-photos-h-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11258053_1586107534976447_1284903766_n.jpg',
+          src: 'https://igcdn-videos-e-19-a.akamaihd.net/hphotos-ak-xfa1/t50.2886-16/11229035_971906422833059_1407736680_n.mp4'
+        },
+        {
+          id: 13,
+          name: 'Shaunhover flatground tricks',
+          cover: 'https://igcdn-photos-h-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11258053_1586107534976447_1284903766_n.jpg',
+          src: 'https://igcdn-videos-e-19-a.akamaihd.net/hphotos-ak-xfa1/t50.2886-16/11229035_971906422833059_1407736680_n.mp4'
+        },
+        {
+          id: 14,
+          name: 'Shaunhover flatground tricks',
+          cover: 'https://igcdn-photos-h-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11258053_1586107534976447_1284903766_n.jpg',
+          src: 'https://igcdn-videos-e-19-a.akamaihd.net/hphotos-ak-xfa1/t50.2886-16/11229035_971906422833059_1407736680_n.mp4'
+        },
+        {
+          id: 15,
+          name: 'Shaunhover flatground tricks',
+          cover: 'https://igcdn-photos-h-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11258053_1586107534976447_1284903766_n.jpg',
+          src: 'https://igcdn-videos-e-19-a.akamaihd.net/hphotos-ak-xfa1/t50.2886-16/11229035_971906422833059_1407736680_n.mp4'
+        },
+        {
+          id: 16,
+          name: 'Shaunhover flatground tricks',
+          cover: 'https://igcdn-photos-h-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11258053_1586107534976447_1284903766_n.jpg',
+          src: 'https://igcdn-videos-e-19-a.akamaihd.net/hphotos-ak-xfa1/t50.2886-16/11229035_971906422833059_1407736680_n.mp4'
+        },
+        {
+          id: 17,
+          name: 'Shaunhover flatground tricks',
+          cover: 'https://igcdn-photos-h-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11258053_1586107534976447_1284903766_n.jpg',
+          src: 'https://igcdn-videos-e-19-a.akamaihd.net/hphotos-ak-xfa1/t50.2886-16/11229035_971906422833059_1407736680_n.mp4'
+        },
+        {
+          id: 18,
+          name: 'Shaunhover flatground tricks',
+          cover: 'https://igcdn-photos-h-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11258053_1586107534976447_1284903766_n.jpg',
+          src: 'https://igcdn-videos-e-19-a.akamaihd.net/hphotos-ak-xfa1/t50.2886-16/11229035_971906422833059_1407736680_n.mp4'
+        },
+      ]
+    }
+  });
+}
