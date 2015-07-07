59381077f4246341e2c3a038ccf893d6542dab70
Hector Leon Zarco Garcia <hector.zarco@king.com>
2015-07-07 15:01:28 +0200

    Defining Faker factories instead of hardcoding values

--- app/mirage/scenarios/default.js
+++ app/mirage/scenarios/default.js
@@ -1,7 +1,4 @@
-export default function(/* server */) {
-
-  // Seed your development database using your factories. This
-  // data will not be loaded in your tests.
-
-  // server.createList('contact', 10);
-}
+export default function(server) {
+  server.createList('user', 10);
+  server.createList('videos', 20);
+};
