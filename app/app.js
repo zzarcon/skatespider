6755a79024acfda1f71f0ec5c37100b8d24e5117
Hector Leon Zarco Garcia <hezarco@gmail.com>
2016-03-14 21:42:16 +0100

    Upgrade config files

--- app/app.js
+++ app/app.js
@@ -1,17 +1,16 @@
 import Ember from 'ember';
-import Resolver from 'ember/resolver';
+import Resolver from 'ember-resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
-var App;
+let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
-Ember.LOG_VERSIONS = false;
 
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
